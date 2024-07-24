5fadbacfa998444e859c87112cff503d11317f1a
Renato28 <renatonobrega87@gmail.com>
2024-07-24 11:18:38 -0300

    Connecting to MySQL with Spring Data, JPA and Hibernate

---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,67 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+		
+		return personRepository.findAll();
+	}
+	
+	public Person findById(Long id) {
+		
+		logger.info("Finding one person!");
+		
+		return personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("Creating one person!");
+		
+		return personRepository.save(person);
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		var entity = personRepository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return personRepository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		
+		logger.info("Deleting one person!");
+		
+		var entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		personRepository.delete(entity);
+	}
+	
+	
+}
